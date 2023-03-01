40f275a328fc42053400d5b6e1aa15b4b4257989
Tatia Burdett <tatiaburdett@gmail.com>
2023-03-01 10:14:58 -0600

    rob v1

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark text-light">
+      <div id="#top" className="container-fluid">
+        <a className="navbar-brand" href="#top">
           Tatia Burdett
         </a>
         <div className="navbar" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 flex-row hstack gap-3">
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">
+              <a className="nav-link" aria-current="page" href="#top">
                 About
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link p-2 btn btn-outline-secondary" href="#top">
                 Resume
               </a>
             </li>
